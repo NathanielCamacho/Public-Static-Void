67cbaccfb78c8fdc3f89e3e51b07cdcab200f6c9
Liaxis <h.arcilla3306@outlook.com>
2024-05-18 02:20:59 +0800

    Update loginlanding.php

--- loginlanding.php
+++ loginlanding.php
@@ -1,9 +1,46 @@
-<?php
-session_start();
-if (!isset($_SESSION['username'])) {
-    header("Location: profile.html");
-    exit();
-}
-echo "Login successful.";
-?>
-<a href="shopnow.html">Continue to product catalog</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    <link rel="stylesheet" href="profile_css.css">
+    <script src="https://kit.fontawesome.com/43b9de10c9.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Almendra+SC&family=Bangers&family=Cinzel+Decorative:wght@400;700;900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Quintessential&family=Satisfy&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="header">
+        <div class="navbar">
+            <a href="homepage.html">
+                <img src="krooked product/white_logo.png" class="logo">
+            </a>
+            <div class="logo_name">The Krooked</div>
+            <nav>
+                <ul>
+                    <li><a href="profile.html" class="profile">
+                        <i class="fa-regular fa-user fa-xl"></i>
+                    </a></li>
+                    <li><a href="shopnow.html" class="cart">
+                        <i class="fa-solid fa-cart-shopping fa-xl"></i>
+                    </a></li>
+                    <li><a href="about.html" class="about">
+                        <i class="fa-regular fa-address-card fa-xl"></i>
+                    </a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div class="main">
+        <?php
+        session_start();
+        if (!isset($_SESSION['username'])) {
+            header("Location: profile.html");
+            exit();
+        }
+        echo "Login successful.";
+        ?>
+        <a href="shopnow.html">Continue to product catalog</a>
+        </body>
+</html>
